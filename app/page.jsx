ccf9d68c32b97a73ecfca636fda909c60dc95237
navacharge <hello@navacharge.com>
2026-05-14 11:21:14 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -81,7 +81,7 @@ url(${heroImage})
           <div
   style={{
     maxWidth: "520px",
-    minHeight: "620px",
+    minHeight: "720px",
     display: "flex",
     flexDirection: "column",
     justifyContent: "space-between",
