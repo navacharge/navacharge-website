8e88d149d8ff30831a29e4c24127bee5e930def8
navacharge <hello@navacharge.com>
2026-05-27 10:51:25 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -455,7 +455,7 @@ url(${heroImage})
     <div className="diagnostic-banner">
 
       <img
-        src="/Diagnostic.png"
+        src="/Diagnostic .png"
         alt="Diagnostic Banner"
       />
 
