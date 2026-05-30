e1e3979f81543217ce32d585869d4c8f49308246
navacharge <hello@navacharge.com>
2026-05-30 12:38:34 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -281,7 +281,7 @@ url(${heroImage})
     .rma-banner img {
       width: 100%;
       height: 100%;
-      object-fit: cover;
+      object-fit: contain;
       background: #07111f;
       display: block;
       border-radius: 22px;
