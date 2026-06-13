5af526c96b816ace568c44074847d61e22f99bd9
navacharge <hello@navacharge.com>
2026-06-13 12:23:43 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -488,7 +488,7 @@ export default function Home() {
 
           .focus-item.item-two {
             top: 450px;
-            right: 70px;
+            right: 110px;
             animation: highlightTwo 12s ease-in-out infinite;
           }
 
