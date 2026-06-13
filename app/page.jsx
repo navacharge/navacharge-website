969e2d485f6d6fdb6311a74861b29c6e3d01e058
navacharge <hello@navacharge.com>
2026-06-13 14:07:04 -0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -482,7 +482,7 @@ export default function Home() {
 
 .focus-item.item-one {
   top: 140px;
-  left: -10px;
+  left: max(28px, calc((1280px - 100vw) / 2 + 28px));
 
   width: min(820px, 62vw);
   padding: 44px;
